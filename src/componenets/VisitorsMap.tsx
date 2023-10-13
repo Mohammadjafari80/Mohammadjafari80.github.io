bf6cc08570a1b7bda0dc6c14a78d6181bfab57ee
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-10-13 16:06:09 +0330

    another try

--- src/componenets/VisitorsMap.tsx
+++ src/componenets/VisitorsMap.tsx
@@ -1,12 +1,29 @@
 // VisitorMap.tsx
-import { Box } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, Center } from '@chakra-ui/react';
+import React, { useEffect, useRef } from 'react';
 
 const VisitorMap: React.FC = () => {
+  const mapRef = useRef(null);  // Create a ref to hold the div element
+  
+{/* <script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=57o2rh561g4&amp;m=0c&amp;c=e4aa42&amp;cr1=ffffff&amp;f=calibri&amp;l=49&amp;cw=3f6ee7&amp;cb=4a9c80" async="async"></script> */}
+
+useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = "//rf.revolvermaps.com/0/0/8.js?i=57o2rh561g4&amp;m=0c&amp;c=e4aa42&amp;cr1=ffffff&amp;f=calibri&amp;l=49&amp;cw=3f6ee7&amp;cb=4a9c80";
+    script.async = true;
+    mapRef.current.appendChild(script);  // Append the script to the div element
+
+    // Optional: Cleanup the script on component unmount
+    return () => {
+      mapRef.current.removeChild(script);
+    };
+  }, []);  // Empty dependency array ensures this useEffect runs once
+
   return (
-    <Box w={'200px'} h={'200px'}>
-      <script type="text/javascript" src="//rf.revolvermaps.com/0/0/3.js?i=54pbp3vvgtd&amp;b=0&amp;s=40&amp;m=2&amp;cl=ffffff&amp;co=010020&amp;cd=aa0000&amp;v0=60&amp;v1=10&amp;r=1" async="async"></script>
-    </Box>
+    <Center>
+      <Flex ref={mapRef} w={'100px'} h={'100px'} mx={'auto'} alignItems={'center'} justify={'center'}></Flex>
+    </Center>
   );
 };
 
