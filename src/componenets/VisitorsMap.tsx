92aa017a572d6ed54719556e76cc4fb58c679559
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-10-13 17:02:43 +0330

    fix positioning

--- src/componenets/VisitorsMap.tsx
+++ src/componenets/VisitorsMap.tsx
@@ -1,32 +1,28 @@
 // VisitorMap.tsx
-import { Box } from '@chakra-ui/react';
+import { Flex, Center } from '@chakra-ui/react';
 import React, { useEffect, useRef } from 'react';
 
 const VisitorMap: React.FC = () => {
-  const mapRef = useRef<HTMLDivElement>(null);  // Specify HTMLDivElement as the ref type
-
-
-//   <script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=57o2rh561g4&amp;m=0c&amp;c=e4aa42&amp;cr1=ffffff&amp;f=calibri&amp;l=49&amp;cw=3f6ee7&amp;cb=4a9c80" async="async"></script>
+  const mapRef = useRef(null);  // Create a ref to hold the div element
+  
+//   <script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=5yrt38bfucu&amp;m=0c&amp;c=e4aa42&amp;cr1=ffffff&amp;f=arial&amp;l=49&amp;cw=3f6ee7&amp;cb=4a9c80" async="async"></script>
   useEffect(() => {
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = "//rf.revolvermaps.com/0/0/3.js?i=54pbp3vvgtd&b=0&s=40&m=2&cl=ffffff&co=010020&cd=aa0000&v0=60&v1=10&r=1";
     script.async = true;
-
-    if (mapRef.current) {
-      mapRef.current.appendChild(script);  // Append the script to the div element only if mapRef.current is not null
-    }
+    mapRef.current.appendChild(script);  // Append the script to the div element
 
     // Optional: Cleanup the script on component unmount
     return () => {
-      if (mapRef.current) {
-        mapRef.current.removeChild(script);  // Remove the script only if mapRef.current is not null
-      }
+      mapRef.current.removeChild(script);
     };
   }, []);  // Empty dependency array ensures this useEffect runs once
 
   return (
-    <Box ref={mapRef} w={'100px'} h={'100px'}></Box>
+    <Center>
+      <Flex ref={mapRef} w={'50px'} h={'50px'} mx={'auto'} alignItems={'center'} justify={'center'}></Flex>
+    </Center>
   );
 };
 
