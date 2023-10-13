38ea9114a277b833fe800c273f5dbb1856aab344
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-10-13 15:32:47 +0330

    fix visitors

--- src/App.tsx
+++ src/App.tsx
@@ -97,7 +97,9 @@ const App: React.FC = () => {
           <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FMohammadjafari80%2FMohammadjafari80.github.io&count_bg=%23D05A45&title_bg=%23373232&icon=&icon_color=%23E7E7E7&title=visits&edge_flat=false" />
         </a>
       </Center>
-      <VisitorMap></VisitorMap>
+      <Center>
+        <VisitorMap></VisitorMap>
+      </Center>
     </Section>
 
     </Box>
--- src/componenets/VisitorsMap.tsx
+++ src/componenets/VisitorsMap.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const VisitorMap: React.FC = () => {
   return (
     <Box w={'200px'} h={'200px'}>
-      <script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=5q8285ra46j&amp;m=0c&amp;c=e4aa42&amp;cr1=ffffff&amp;f=arial&amp;l=49&amp;cw=3f6ee7&amp;cb=4a9c80" async="async"></script>
+      <script type="text/javascript" src="//rf.revolvermaps.com/0/0/3.js?i=54pbp3vvgtd&amp;b=0&amp;s=40&amp;m=2&amp;cl=ffffff&amp;co=010020&amp;cd=aa0000&amp;v0=60&amp;v1=10&amp;r=1" async="async"></script>
     </Box>
   );
 };
