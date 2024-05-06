0ffae349a3194c1c60d2d720eeb464badbdec457
Mohammad Jafari <mamad.jafari91@gmail.com>
2024-05-06 16:53:36 +0330

    add scholar

--- src/componenets/ContactLinks.tsx
+++ src/componenets/ContactLinks.tsx
@@ -1,68 +1,88 @@
 import { Grid, Tag } from "@chakra-ui/react";
-import { FaEnvelope, FaFilePdf, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaEnvelope, FaFilePdf, FaGithub, FaLinkedin, FaUserGraduate } from 'react-icons/fa';
 
 const ContactLinks = () => {
   return (
     <Grid
-      templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(auto-fit, minmax(100px, 1fr))" }}  // 2x2 grid for base and 1x4 grid for md and above
-      gap={{ base: "1", md: "3" }}  // Adjust gap between tags
-    >
-      <Tag
-        borderRadius="md"
-        fontSize="sm"
-        fontWeight="bold"
-        style={{ backgroundColor: hexToRgba("#e4aa42", 0.5) }}
-        m={1}
-        p={2}
-        as="a"
-        href="mailto:mohammad.jafari.varnous.2001@gmail.com"
-      >
-        <FaEnvelope style={{ marginRight: '12px' }} />
-        Email
-      </Tag>
+  templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(6, 1fr)" }}  // 2 columns on base, 3 on md and above
+  gap={{ base: "1", md: "1" }}
+>
+  <Tag
+    borderRadius="md"
+    fontSize="sm"
+    fontWeight="bold"
+    style={{ backgroundColor: hexToRgba("#e4aa42", 0.5) }}
+    m={1}
+    p={2}
+    as="a"
+    href="mailto:mohammad.jafari.varnous.2001@gmail.com"
+    gridColumn={{ base: "span 1", md: "span 2" }}
+  >
+    <FaEnvelope style={{ marginRight: '12px' }} />
+    Email
+  </Tag>
 
-      <Tag
-        borderRadius="md"
-        fontSize="sm"
-        fontWeight="bold"
-        style={{ backgroundColor: hexToRgba("#d05a45", 0.5) }}  // Color updated to match LinkedIn tag
-        m={1}
-        p={2}
-        as="a"
-        href="/CV_MohammadJafari.pdf"
-      >
-        <FaFilePdf style={{ marginRight: '12px' }} />
-        CV
-      </Tag>
+  <Tag
+    borderRadius="md"
+    fontSize="sm"
+    fontWeight="bold"
+    style={{ backgroundColor: hexToRgba("#d05a45", 0.5) }}
+    m={1}
+    p={2}
+    as="a"
+    href="/CV_MohammadJafari.pdf"
+    gridColumn={{ base: "span 1", md: "span 2" }}
+  >
+    <FaFilePdf style={{ marginRight: '12px' }} />
+    CV
+  </Tag>
 
-      <Tag
-        borderRadius="md"
-        fontSize="sm"
-        fontWeight="bold"
-        style={{ backgroundColor: hexToRgba("#4a9c80", 0.5) }}
-        m={1}
-        p={2}
-        as="a"
-        href="https://github.com/mohammadjafari80"
-      >
-        <FaGithub style={{ marginRight: '12px' }} />
-        Github
-      </Tag>
+  <Tag
+    borderRadius="md"
+    fontSize="sm"
+    fontWeight="bold"
+    style={{ backgroundColor: hexToRgba("#4a9c80", 0.5) }}
+    m={1}
+    p={2}
+    as="a"
+    href="https://github.com/mohammadjafari80"
+    gridColumn={{ base: "span 1", md: "span 2" }}
+  >
+    <FaGithub style={{ marginRight: '12px' }} />
+    Github
+  </Tag>
 
-      <Tag
-        borderRadius="md"
-        fontSize="sm"
-        fontWeight="bold"
-        style={{ backgroundColor: hexToRgba("#3f6ee7", 0.5) }}
-        m={1}
-        p={2}
-        as="a"
-        href="https://www.linkedin.com/in/mohammadjafari01/"
-      >
-        <FaLinkedin style={{ marginRight: '12px' }} />
-        LinkedIn
-      </Tag>
-    </Grid>
+  <Tag
+    borderRadius="md"
+    fontSize="sm"
+    fontWeight="bold"
+    style={{ backgroundColor: hexToRgba("#8746dc", 0.5) }}
+    m={1}
+    p={2}
+    as="a"
+    href="https://scholar.google.com/citations?hl=en&authuser=1&user=S8MWCKMAAAAJ"
+    gridColumn={{ base: "span 1", md: "span 3" }}
+  >
+    <FaUserGraduate style={{ marginRight: '12px' }} />
+    Scholar
+  </Tag>
+
+  <Tag
+    borderRadius="md"
+    fontSize="sm"
+    fontWeight="bold"
+    style={{ backgroundColor: hexToRgba("#3f6ee7", 0.5) }}
+    m={1}
+    p={2}
+    as="a"
+    href="https://www.linkedin.com/in/mohammadjafari01/"
+    gridColumn={{ base: "1 / -1", md: "span 3" }}  // Span full width in base, single column in md
+  >
+    <FaLinkedin style={{ marginRight: '12px' }} />
+    LinkedIn
+  </Tag>
+</Grid>
+
   );
 };
 
