8f9e0c6791824031c8af96ea1475f8898244bcec
Mohammad Jafari <mamad.jafari91@gmail.com>
2025-01-21 22:23:31 +0330

    update bio and loading time

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,34 +1,29 @@
-import {Box } from "@chakra-ui/react";
-import TextEffect from "../GPT"
+import { Box } from "@chakra-ui/react";
+import TextEffect from "../GPT";
 
 const AboutMe = () => {
+  
+  const text = `I hold a Bachelor’s degree in Computer Engineering from **Sharif University of Technology**, with a focus on **AI safety** and **building reliable systems**. My work centers on developing **scalable and trustworthy algorithms**, particularly in the areas of **safety for large language models (LLMs)** and **AI alignment**.
+  ‎ 
+  I am particularly interested in **LLM reasoning**, **multimodal models**, and **ensuring the safety and reliability of AI systems**. My goal is to contribute to creating **secure and resilient AI technologies** that address critical challenges while prioritizing privacy and ethical considerations.`
+
   return (
     <Box>
-
-      <TextEffect text={`I hold a Bachelor's degree in Computer Engineering from **Sharif University of Technology**, with a strong focus on **AI safety**, **optimization**, and **robust systems**. My work centers on developing **scalable** and **trustworthy algorithms**, especially in the areas of **safety for large language models (LLMs)** and **privacy-preserving AI**.
-‎ 
-I am particularly interested in *computer vision*, *natural language processing* (NLP), *multi-level optimization*, and *AI privacy*. My goal is to contribute to creating **efficient**, **resilient**, and **secure AI systems** that address important challenges in technology while prioritizing privacy and reliability.
-
-`} minTypingDelay={10} maxTypingDelay={10} notDisplayCaretAfterFinishes thinkingDelay={100}/>
-      {/* <Text>
-        I hold a Bachelor’s degree in Computer Engineering from 
-        <Text as="b"> Sharif University of Technology</Text>, with a strong focus on 
-        <Text as="b"> AI safety</Text>, <Text as="b"> optimization</Text>, and 
-        <Text as="b"> robust systems</Text>. My work centers on developing scalable and 
-        <Text as="b"> trustworthy algorithms</Text>, especially in the areas of 
-        <Text as="b"> safety for large language models (LLMs)</Text> and 
-        <Text as="b"> privacy-preserving AI</Text>.
-      </Text>
-      <Text mt={4}>
-        I am particularly interested in <Text as="i">computer vision</Text>, 
-        <Text as="i">natural language processing</Text> (NLP), 
-        <Text as="i">multi-level optimization</Text>, and <Text as="i">AI privacy</Text>. 
-        My goal is to contribute to creating <Text as="b"> efficient</Text>, 
-        <Text as="b"> resilient</Text>, and <Text as="b"> secure AI systems </Text> 
-        that address important challenges in technology while prioritizing privacy and reliability.
-      </Text> */}
+      <Box
+        minHeight={'300px'}
+        overflow="hidden" // Prevent text from overflowing
+      >
+        <TextEffect
+          text={text}
+          minTypingDelay={1}
+          maxTypingDelay={1}
+          notDisplayCaretAfterFinishes
+          thinkingDelay={100}
+        />
+      </Box>
     </Box>
   );
 };
+
 
 export default AboutMe;
